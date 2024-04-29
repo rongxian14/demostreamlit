35a5a8fde22d304b32ac8359700ed6d86bd925f5
LEE RONG XIAN <102236116+rongxian14@users.noreply.github.com>
2024-04-29 15:33:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,17 @@ plt.plot(data["x"][:subset_size], data["y"][:subset_size], "-o")
 st.pyplot(plt)
 # Add a text input for comments or notes
 notes = st.text_area("Add your comments or notes here")
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
